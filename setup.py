6029f75fb7bf02c45dc329e4844bb9f89d599f8f
salihsefer36 <sefersalih017@gmail.com>
2026-02-04 20:10:12 +0300

    1.0.1 for PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="shadowstep",
-    version="1.0.0",
+    version="1.0.1",
     author="Salih Sefer",
     author_email="sefersalih017@gmail.com",
     description="Advanced Anti-Forensics & System Artifact Management Tool",
